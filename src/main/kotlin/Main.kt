336912d1d7952c61d9ac4764aa363ec53faa1a22
20098630 <woodspeter37@gmail.com>
2023-01-26 16:27:29 +0000

    Changed code to allow a menu to pop up and select options

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,14 +12,47 @@ val cycleWork = 54.33
 
 
 fun main(args: Array<String>) {
-    payslip()
+
+    var input: Int
+
+    do {
+        input = menu()
+        when (input) {
+            1 -> println("Monthly Salary ${(monthlySal())}")
+            2 -> println("Monthly PRSI: ${(monthlyPRSI())}")
+            3 -> println("Monthly PAYE: ${(monthlyPAYE())}")
+            4 -> println("Monthly Gross Pay: ${(monthlyGross())}")
+            5 -> println("Monthly Total Deductions: ${(deductions())}")
+            6 -> println("Monthly Net Pay: ${(netPay())}")
+            7 -> println(getPaySlip())
+            -1 -> println("Exiting App")
+            else -> println("Invalid Option")
+        }
+        println()
+    } while (input != -1)
 }
 
 
-fun payslip(){
 
-    println(
-        """
+fun menu(): Int {
+    print("""
+        Employee Menu for $(getFullName()}
+          1. Monthly Salary
+          2. Monthly PRSI
+          3. Monthly PAYE
+          4. Monthly Gross Pay
+          5. Monthly Total Deductions
+          6. Monthly Net Pay
+          7. Full Payslip
+          -1. Exit Menu
+          Enter option :  """)
+    return readLine()!!.toInt()
+}
+
+fun getPaySlip(): String {
+
+      return  """
+          ${getFullName()}
     ----------------------------------------------------------------------------
     |                          Monthly Payslip                                 |
     |--------------------------------------------------------------------------|
@@ -31,21 +64,29 @@ fun payslip(){
     |     PAYMENT DETAILS                   DEDUCTION DETAILS                  |
     |                                                                          |
     |--------------------------------------------------------------------------|
-    |     Salary: ${"%.2f".format(grossSal/12)}                   PAYE: ${"%.2f".format(monthlySal()*(PAYE/100))}                      |
-    |     Bonus:  ${"%.2f".format(bonus/12)}                    PRSI: ${"%.2f".format(monthlySal() * (PRSI / 100))}                       |
+    |     Salary: ${"%.2f".format(monthlySal())}                   PAYE: ${"%.2f".format(monthlyPAYE())}                      |
+    |     Bonus:  ${"%.2f".format(bonus/12)}                    PRSI: ${"%.2f".format(monthlyPRSI())}                       |
     |                                       Cycle To Work: ${"%.2f".format(cycleWork)}               |
     |--------------------------------------------------------------------------|
-    |     Gross: ${("%.2f".format((grossSal/12)+(bonus/12)))}                    Total Deductions: ${"%.2f".format(deductions())}          |
+    |     Gross: ${("%.2f".format(monthlyGross()))}                    Total Deductions: ${"%.2f".format(deductions())}          |
     |--------------------------------------------------------------------------|
     |                         NET PAY: ${"%.2f".format(netPay())}                                 |
     ----------------------------------------------------------------------------
-    """)
-
-
+    """
 
 }
 
 fun monthlySal()= grossSal/12
-fun netPay() = ((grossSal/12)+(bonus/12)) - ((monthlySal()*(PAYE/100))+(monthlySal() * (PRSI / 100))+(cycleWork))
+fun monthlyPRSI() = monthlySal() * (PRSI / 100)
+fun monthlyPAYE() = monthlySal()*(PAYE/100)
+fun monthlyGross() = monthlySal()+(bonus/12)
 fun deductions() = (monthlySal()*(PAYE/100))+(monthlySal() * (PRSI / 100))+(cycleWork)
+fun netPay() = ((grossSal/12)+(bonus/12)) - ((monthlySal()*(PAYE/100))+(monthlySal() * (PRSI / 100))+(cycleWork))
 
+
+
+
+fun getFullName() = when(Gender){
+        "m" -> "Mr. $firstname $surname"
+        "f" -> "Mrs. $firstname $surname"
+        else -> "$firstname $surname"}
